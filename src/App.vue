58dc651f067791b902ca0ec19f5dae84b28d13a0
Alexander Bulanov <alex-bulanov.mail@yandex.ru>
2023-04-18 11:23:07 +0300

    Forms Intro & Setup

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<script setup>
+import SignupForm from './components/SignupForm.vue'
+</script>
+
+<template>
+  <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
+  <div>
+    <SignupForm />
+  </div>
+</template>
+
+<style scoped>
+.logo {
+  display: block;
+  margin: 0 auto 2rem;
+}
+</style>
---
+++ src/components/SignupForm.vue
@@ -0,0 +1,39 @@
+<script setup></script>
+
+<template>
+  <form>
+    <label>Email:</label>
+    <input type="email" required />
+  </form>
+</template>
+
+<style scoped>
+form {
+  margin: 30px auto;
+  border-radius: 10px;
+  max-width: 420px;
+  padding: 40px;
+  text-align: left;
+  background-color: #ffffff;
+}
+
+label {
+  margin: 25px 0 15px;
+  display: inline-block;
+  font-size: 0.6em;
+  font-weight: bold;
+  text-transform: uppercase;
+  letter-spacing: 1px;
+  color: #aaaaaa;
+}
+
+input {
+  box-sizing: border-box;
+  border: none;
+  border-bottom: 1px solid #dddddd;
+  display: block;
+  width: 100%;
+  padding: 10px 6px;
+  color: #555555;
+}
+</style>
